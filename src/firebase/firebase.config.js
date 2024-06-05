a9f14cd4b085eae031411cc1b9e95d845f182090
shahidx0x <dev.imshahid@gmail.com>
2024-06-05 17:15:15 +0600

    update firebase.config.js

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAL42gZk4ptVTv5TCtWz3EXo8BNvwIyfEg",
-  authDomain: "sastho-seba-one.firebaseapp.com",
-  projectId: "sastho-seba-one",
-  storageBucket: "sastho-seba-one.appspot.com",
-  messagingSenderId: "464531977256",
-  appId: "1:464531977256:web:4fb9c96596bf7535e6b11d",
-  measurementId: "G-KCW2HMFVXW",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 export default firebaseConfig;
+  
